a154388b498df81b25bb01d5bdaff8602f8f1254
Zachary Wilson <zw96042@gmail.com>
2026-08-04 19:15:00 +0000

    feat: Register compatibility planner adapters part 1

--- src/shared/planners/index.ts
+++ src/shared/planners/index.ts
@@ -2,18 +2,18 @@ import type { TrajectoryPlanner, TrajectoryPlannerId } from "../types";
 import type { PlannerInput, PlannerResult } from "../types";
 import { optimizedTrajectoryPlanner } from "./optimizedTrajectory";
 import { profiledSplinePlanner } from "./profiledSpline";
+import { labviewBezierPlanner, labviewClothoidPlanner } from "./labviewCompatible";
+import { applyStationaryActions } from "./stationaryActions";
+import { applyRotationPriority } from "./rotationPriority";
 
 export const planners: Record<TrajectoryPlannerId, TrajectoryPlanner> = {
   profiledSpline: profiledSplinePlanner,
   optimizedTrajectory: optimizedTrajectoryPlanner,
+  labviewBezier: labviewBezierPlanner,
+  labviewClothoid: labviewClothoidPlanner,
 };
 
 export function getPlanner(id: TrajectoryPlannerId): TrajectoryPlanner {
   const planner = planners[id];
-  return planner;
-}
-
-export async function generateTrajectory(input: PlannerInput): Promise<PlannerResult> {
-  const planner = getPlanner(input.plannerId ?? "profiledSpline");
-  return planner.generate(input);
-}
+  return {
+    id: planner.id,
