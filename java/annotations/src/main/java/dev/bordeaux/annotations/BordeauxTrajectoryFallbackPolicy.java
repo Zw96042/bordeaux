d24d332eae84cf4d9293b9c6744c1693739f1ee2
Zachary Wilson <zw96042@gmail.com>
2026-09-08 17:20:29 -0500

    refactor: Remove retired BordeauxCommand support files

--- java/annotations/src/main/java/dev/bordeaux/annotations/BordeauxTrajectoryFallbackPolicy.java
+++
@@ -1,8 +0,0 @@
-package dev.bordeaux.annotations;
-
-/** Closed failure policy declared by a robot-owned trajectory generator. */
-public enum BordeauxTrajectoryFallbackPolicy {
-    UNSPECIFIED,
-    SAFE_STOP_ONLY,
-    VALIDATED_BRANCH
-}
--- java/annotations/src/main/java/dev/bordeaux/annotations/BordeauxTrajectoryPreview.java
+++
@@ -1,7 +0,0 @@
-package dev.bordeaux.annotations;
-
-/** Closed preview contract for a robot-owned trajectory generator. */
-public enum BordeauxTrajectoryPreview {
-    UNSPECIFIED,
-    RUNTIME_DYNAMIC
-}
