14971f0a3552b2d2c4dbca408d85b2f771173967
Zachary Wilson <zw96042@gmail.com>
2026-09-08 17:20:27 -0500

    build: Remove retired benchmark adapter scaffolding

--- benchmarks/adapters/pathplanner/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://frcmaven.wpi.edu/artifactory/release")
-    }
-}
-
-rootProject.name = "bordeaux-pathplanner-adapter"
